48727baf0dd805de9107f944be612ec88569e4f1
tfleming <tfleming26@hotmail.com>
2018-04-23 20:30:44 -0600

    Add placeorder method to vendor class

--- Acme.Biz/Vendor.cs
+++ Acme.Biz/Vendor.cs
@@ -17,6 +17,39 @@ namespace Acme.Biz
         public string Email { get; set; }
 
         /// <summary>
+        /// Sends a product order to the vendor.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="quantity"></param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
+            var success = false;
+
+            var orderText = "Order from Acme, Inc" + System.Environment.NewLine +
+                            "Product: " + product.ProductCode +
+                                            System.Environment.NewLine +
+                            "Quantity: " + quantity;
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText,
+                                                                     this.Email);
+
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult ;
+        }
+
+        /// <summary>
         /// Sends an email to welcome a new vendor.
         /// </summary>
         /// <returns></returns>
