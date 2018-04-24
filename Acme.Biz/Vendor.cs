7d1d548b467546cfee105c22cf19bf97bd987fad
tfleming <tfleming26@hotmail.com>
2018-04-24 09:27:54 -0600

    Add additional placeorder methods with overloading and tests

--- Acme.Biz/Vendor.cs
+++ Acme.Biz/Vendor.cs
@@ -50,6 +50,96 @@ namespace Acme.Biz
         }
 
         /// <summary>
+        /// Sends a product order to the vendor.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="quantity"></param>
+        /// <param name="deliverBy"></param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity,
+                                          DateTimeOffset? deliverBy)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
+            var success = false;
+
+            var orderText = "Order from Acme, Inc" + Environment.NewLine +
+                            "Product: " + product.ProductCode +
+                                            Environment.NewLine +
+                            "Quantity: " + quantity;
+
+            if (deliverBy.HasValue)
+            {
+                orderText += Environment.NewLine +
+                             "Deliver By: " + deliverBy.Value.ToString("d");
+            }
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText,
+                                                                     this.Email);
+
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+        }
+
+        /// <summary>
+        /// Sends a product order to the vendor.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="quantity"></param>
+        /// <param name="deliverBy"></param>
+        /// <param name="instructions"></param>
+        /// <returns></returns>
+        public OperationResult PlaceOrder(Product product, int quantity,
+                                          DateTimeOffset? deliverBy,
+                                          string instructions)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (quantity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+
+            var success = false;
+
+            var orderText = "Order from Acme, Inc" + Environment.NewLine +
+                            "Product: " + product.ProductCode +
+                                            Environment.NewLine +
+                            "Quantity: " + quantity;
+
+            if (deliverBy.HasValue)
+            {
+                orderText += Environment.NewLine +
+                             "Deliver By: " + deliverBy.Value.ToString("d");
+            }
+
+            if (!String.IsNullOrWhiteSpace(instructions))
+            {
+                orderText += Environment.NewLine +
+                             "Instructions: " + instructions;
+            }
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Order", orderText,
+                                                                     this.Email);
+
+            if (confirmation.StartsWith("Message sent:"))
+            {
+                success = true;
+            }
+
+            var operationResult = new OperationResult(success, orderText);
+            return operationResult;
+        }
+
+        /// <summary>
         /// Sends an email to welcome a new vendor.
         /// </summary>
         /// <returns></returns>
--- Tests/Acme.BizTests/VendorTests.cs
+++ Tests/Acme.BizTests/VendorTests.cs
@@ -74,6 +74,44 @@ namespace Acme.Biz.Tests
         }
 
         [TestMethod()]
+        public void PlaceOrder_3Parameters()
+        {
+            // Arrange
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true,
+                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12" +
+                "\r\nDeliver By: 10/25/2015");
+
+            // Act
+            var actual = vendor.PlaceOrder(product, 12,
+                new DateTimeOffset(2015, 10, 25, 0, 0, 0, new TimeSpan(-7, 0, 0)));
+
+            // Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        public void PlaceOrder_4Parameters()
+        {
+            // Arrange
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true,
+                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12" +
+                "\r\nDeliver By: 10/25/2015" + "\r\nInstructions: Please don't bend");
+
+            // Act
+            var actual = vendor.PlaceOrder(product, 12,
+                new DateTimeOffset(2015, 10, 25, 0, 0, 0, new TimeSpan(-7, 0, 0)), ("Please don't bend"));
+
+            // Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
         [ExpectedException(typeof(ArgumentNullException))]
         public void PlaceOrder_NullProduct_Exception()
         {
