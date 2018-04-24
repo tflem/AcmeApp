4f4d77abefc661fe8eaef395356c06e22c98d193
tfleming <tfleming26@hotmail.com>
2018-04-24 08:45:52 -0600

    Add tests for placeorder method in vendor class

--- Tests/Acme.BizTests/VendorTests.cs
+++ Tests/Acme.BizTests/VendorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
 
 namespace Acme.Biz.Tests
 {
@@ -54,6 +55,36 @@ namespace Acme.Biz.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            // Arrange
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true,
+                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12");
+
+            // Act
+            var actual = vendor.PlaceOrder(product, 12);
+
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrder_NullProduct_Exception()
+        {
+            // Arrange
+            var vendor = new Vendor();
+
+            // Act
+            var actual = vendor.PlaceOrder(null, 12);
+
+            // Assert
+            // Expected exception            
         }
     }
 }
